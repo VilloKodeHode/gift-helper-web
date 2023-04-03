f1e37626da2fc79ef9dbf70e4603f871a3600838
Joakim Villo <villokodehode@gmail.com>
2023-04-03 14:51:49 +0200

    styled

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 import "./App.css";
 import gifts from "./data/gifts";
 import questions from "./data/questions";
-import GiftHelperButton from "./components/Buttons";
+import GiftHelperButton, {
+  GiftHelperResetButton,
+  GiftHelperSearchButton,
+} from "./components/Buttons";
 
 function App() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -13,8 +16,17 @@ function App() {
     setCurrentQuestionIndex(currentQuestionIndex + 1);
   };
 
+  const openSearchResults = (giftName) => {
+    const occasion = questions[0].options[answers[questions[0].id] - 1].text;
+    const gender = questions[1].options[answers[questions[1].id] - 1].text;
+    const ageRange = questions[2].options[answers[questions[2].id] - 1].text;
+    const searchQuery = `${giftName} ${occasion} ${gender} ${ageRange}`;
+    const encodedQuery = encodeURIComponent(searchQuery);
+    window.open(`https://www.google.com/search?q=${encodedQuery}`);
+  };
+
   const getGiftSuggestion = () => {
-    const matchingGift = gifts.find(
+    const matchingGifts = gifts.filter(
       (gift) =>
         gift.occasion ===
           questions[0].options[answers[questions[0].id] - 1].text &&
@@ -23,7 +35,43 @@ function App() {
         gift.ageRange ===
           questions[2].options[answers[questions[2].id] - 1].text
     );
-    return matchingGift ? matchingGift.gift : "No gift found";
+    if (matchingGifts.length === 0) {
+      return "No gift found";
+    } else {
+      return (
+        <>
+          {matchingGifts.map((giftsObject) => (
+            <ul className="grid gap-4">
+              {giftsObject.gift.map((gift) => (
+                <div
+                  className="grid items-center justify-between grid-flow-col text-left"
+                  key={gift.gift}
+                >
+                  <li>{gift.gift}</li>
+                  {gift.gift.indexOf("Nothing") === -1 && (
+                    <GiftHelperSearchButton
+                      onClick={() =>
+                        openSearchResults(
+                          gift.gift,
+                          questions[0].options[answers[questions[0].id] - 1]
+                            .text,
+                          questions[1].options[answers[questions[1].id] - 1]
+                            .text,
+                          questions[2].options[answers[questions[2].id] - 1]
+                            .text
+                        )
+                      }
+                    >
+                      Search
+                    </GiftHelperSearchButton>
+                  )}
+                </div>
+              ))}
+            </ul>
+          ))}
+        </>
+      );
+    }
   };
 
   const resetQuiz = () => {
@@ -54,13 +102,15 @@ function App() {
         </>
       )}
       {!currentQuestion && (
-        <div className="grid justify-center items-center gap-12">
+        <div className="grid items-center justify-between gap-12">
           <>
-            <p className="resultText">We recommend getting this as a gift:</p>
-            <p className="text-5xl">{getGiftSuggestion()}</p>
-            <GiftHelperButton className="m-auto" onClick={resetQuiz}>
+            <p className="text-3xl">We recommend getting this as a gift:</p>
+
+            <p className="text-3xl md:text-4xl">{getGiftSuggestion()}</p>
+
+            <GiftHelperResetButton className="m-auto" onClick={resetQuiz}>
               Start Over
-            </GiftHelperButton>
+            </GiftHelperResetButton>
           </>
         </div>
       )}
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,7 +2,29 @@ export default function GiftHelperButton({ children, onClick, className }) {
   return (
     <button
       onClick={onClick}
-      className={`flex px-16 m-4 font-semibold shadow-md h-16 w-fit text-JWC-white font-Roboto shadow-JWC-tertiary btn-ghost btn bg-gradient-to-r from-JWC-primary to-JWC-secondary rounded-3xl ${className}`}
+      className={`flex md:px-16 md:m-4 m-1 font-semibold shadow-md md:h-16 w-fit text-JWC-white font-Roboto shadow-JWC-tertiary btn-ghost btn bg-gradient-to-r from-JWC-primary to-JWC-secondary rounded-3xl ${className}`}
+    >
+      <>{children}</>
+    </button>
+  );
+}
+
+export function GiftHelperSearchButton({ children, onClick, className }) {
+  return (
+    <button
+      onClick={onClick}
+      className={`flex px-8 m-4 font-semibold shadow-md h-8 w-fit text-JWC-white font-Roboto shadow-JWC-primary btn-ghost btn bg-gradient-to-r to-JWC-tertiary from-JWC-secondary rounded-3xl ${className}`}
+    >
+      <>{children}</>
+    </button>
+  );
+}
+
+export function GiftHelperResetButton({ children, onClick, className }) {
+  return (
+    <button
+      onClick={onClick}
+      className={`flex px-16 font-semibold shadow-md h-16 w-fit text-JWC-white font-Roboto shadow-JWC-tertiary btn-ghost btn bg-gradient-to-r from-JWC-primary to-JWC-secondary rounded-3xl ${className}`}
     >
       <>{children}</>
     </button>
